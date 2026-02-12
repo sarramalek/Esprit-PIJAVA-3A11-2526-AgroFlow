825ef2e6b2cc0e182012239fbc2ff4541014289e
sarra <benmaleksarra62@gmail.com>
2026-02-12 17:50:15 +0100

    structure JavaFX

---
+++ src/main/java/org/esprit_workshop3a11example/integration/HelloController.java
@@ -0,0 +1,14 @@
+package org.esprit_workshop3a11example.integration;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/org/esprit_workshop3a11example/integration/Launcher.java
@@ -0,0 +1,9 @@
+package org.esprit_workshop3a11example.integration;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
