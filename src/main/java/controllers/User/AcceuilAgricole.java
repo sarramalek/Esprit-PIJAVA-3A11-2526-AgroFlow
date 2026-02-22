34e6c41264805d76091a6496d21048a36f7d4774
sarra <benmaleksarra62@gmail.com>
2026-02-22 02:33:17 +0100

    ajout des fonctionnalitees rechereche triee  filtre PDF et stats pour les Interfaces des Agricoles et Employee ainsi que l'option d'authentifiwation 2  facteurs

--- src/main/java/controllers/User/AcceuilAgricole.java
+++ src/main/java/controllers/User/AcceuilAgricole.java
@@ -9,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.geometry.Pos;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import models.User.Abonnements;
 import models.User.Personne;
@@ -264,8 +265,17 @@ public class AcceuilAgricole {
 
     // Ajouter cette méthode handleAPropos()
     @FXML
-    private void handleAPropos(MouseEvent event) {
-        navigateTo(event,"/UsersInterface/ProfilAgricole.fxml","ddd");
+    private void handleAPropos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UsersInterface/ProfilEmplye.fxml"));
+            Parent root = loader.load();
+            ProfilEmploye ctrl = loader.getController();
+            if (ctrl != null && currentUser != null) ctrl.setCurrentUser(currentUser);
+            Stage s = new Stage();
+            s.setTitle("Mon Profil"); s.setScene(new Scene(root));
+            s.setResizable(true); s.initModality(Modality.APPLICATION_MODAL);
+            s.centerOnScreen(); s.showAndWait();
+        } catch (IOException e) { showError("Erreur"+ e.getMessage()); }
     }
     private void navigateTo(MouseEvent event, String fxmlPath, String title) {
         try {
